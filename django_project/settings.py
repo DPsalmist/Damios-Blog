9df1e6005f3cfa96159f6e2ef2624412714b131f
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-20 04:22:36 +0100

    Set Debug False

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r64---(+3iojww)731hkr06!@jil_hho=qa%k7goybo7_8=_v3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['damiosblog.herokuapp.com', '127.0.0.1']
 # Application definition
