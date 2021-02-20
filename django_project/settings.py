a5179f653dff8ab1e028aa30f4b33872c3a65c47
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-20 04:14:38 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,13 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( 
+STATICFILES_DIR = ( 
     os.path.join(BASE_DIR, 'static')
 )
 
+# Configure Django App for Heroku.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
