993629d05ca872ca754a79412c95453a7507cf33
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-02-20 03:14:33 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +123,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
